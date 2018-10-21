d421a363992ce2f0bc7af6bd6bc8ebde0aca3362
Pranav Ahuja <pranav.ahuja3@gmail.com>
2018-10-21 18:30:11 +0530

    backend code

--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+class blog(models.Model):
+    title=models.CharField(max_length=30)
+    datentime=models.DateTimeField(auto_now=False)
+    image = models.ImageField(upload_to='images/') 
+    body= models.TextField()
 # Create your models here.
