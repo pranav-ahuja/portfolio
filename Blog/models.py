f5efa727f705040a7520208a5b37be43a2749f23
Pranav Ahuja <pranav.ahuja3@gmail.com>
2018-10-23 12:32:46 +0530

    This is my Portfolio

--- Blog/models.py
+++ Blog/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 class blog(models.Model):
-    title=models.CharField(max_length=30)
+    title=models.CharField(max_length=250)
     datentime=models.DateTimeField(auto_now=False)
     image = models.ImageField(upload_to='images/') 
     body= models.TextField()
+    def summary(self):
+        return self.body[:50]
+    def pub_date_pretty(self):
+        return self.datentime.strftime('%b %e %Y')
+    def __str__(self):
+        return self.title
 # Create your models here.
